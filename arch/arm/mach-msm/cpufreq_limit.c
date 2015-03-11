18eaaa9fa841905186476932222bd86074425539
Zhao Wei Liew <zhaoweiliew@gmail.com>
2015-03-11 20:00:05 +0800

    msm: cpufreq_limit: Schedule suspend and resume works
    Signed-off-by: Zhao Wei Liew <zhaoweiliew@gmail.com>

--- arch/arm/mach-msm/cpufreq_limit.c
+++ arch/arm/mach-msm/cpufreq_limit.c
@@ -25,39 +25,50 @@ static uint32_t suspend_max_freq __read_mostly = 1026000;
 module_param(suspend_max_freq, uint, 0644);
 
 static uint32_t previous_max_freq = MSM_CPUFREQ_NO_LIMIT;
+static struct work_struct suspend_work, resume_work;
 
-static void cpufreq_limit_early_suspend(struct early_suspend *h)
+static void cpufreq_limit_suspend(struct work_struct *work)
 {
-	int cpu = 0;
+	unsigned int cpu = 0;
 
-	previous_max_freq = cpufreq_quick_get_max(0);
+	previous_max_freq = cpufreq_quick_get_max(cpu);
 
 	if (unlikely(previous_max_freq <= suspend_max_freq))
 		return;
 
 	for_each_possible_cpu(cpu) {
 		msm_cpufreq_set_freq_limits(cpu, MSM_CPUFREQ_NO_LIMIT,
-                        suspend_max_freq ? suspend_max_freq : MSM_CPUFREQ_NO_LIMIT);
+			suspend_max_freq ? suspend_max_freq : MSM_CPUFREQ_NO_LIMIT);
 
-                pr_info("%s: CPU%d max freq limited to: %d Hz\n",
+		pr_info("%s: CPU%d max freq limited to: %d Hz\n",
 			CPUFREQ_LIMIT, cpu, suspend_max_freq);
 	}
 }
 
-static void cpufreq_limit_late_resume(struct early_suspend *h)
+static void cpufreq_limit_resume(struct work_struct *work)
 {
-	int cpu = 0;
+	unsigned int cpu = 0;
 
 	if (unlikely(previous_max_freq <= suspend_max_freq))
 		return;
 
 	for_each_possible_cpu(cpu) {
 		msm_cpufreq_set_freq_limits(cpu, MSM_CPUFREQ_NO_LIMIT,
-                        previous_max_freq);
+			previous_max_freq);
 
 		pr_info("%s: CPU%d max frequency restored to %d Hz\n",
 			CPUFREQ_LIMIT, cpu, previous_max_freq);
 	}
+}
+
+static void cpufreq_limit_early_suspend(struct early_suspend *h)
+{
+	schedule_work(&suspend_work);
+}
+
+static void cpufreq_limit_late_resume(struct early_suspend *h)
+{
+	schedule_work(&resume_work);
 }
 
 static struct early_suspend cpufreq_limit_suspend_handler = {
@@ -71,6 +82,9 @@ static int __init cpufreq_limit_init(void)
 	pr_info("%s: Initialized", CPUFREQ_LIMIT);
 
 	register_early_suspend(&cpufreq_limit_suspend_handler);
+
+	INIT_WORK(&suspend_work, cpufreq_limit_suspend);
+	INIT_WORK(&resume_work, cpufreq_limit_resume);
 
 	return 0;
 }
